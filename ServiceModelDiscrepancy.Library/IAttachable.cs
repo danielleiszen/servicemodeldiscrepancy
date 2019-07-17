1be21f17a69bb7de72ea9a3fce34a681f3bb1407
Daniel Leiszen <individualhero@WINDOWSLIVE.COM>
2019-07-17 17:07:13 +0200

    initial

---
+++ ServiceModelDiscrepancy.Library/IAttachable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ServiceModelDiscrepancy.Library
+{
+    public interface IAttachable
+    {
+        void Attach(OperationContext context);
+        void Detach(OperationContext context);
+    }
+}
---
+++ ServiceModelDiscrepancy/ServiceImplementation.cs
@@ -0,0 +1,22 @@
+﻿using ServiceModelDiscrepancy.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceModelDiscrepancy
+{
+    public class ServiceImplementation : IAttachable
+    {
+        void IAttachable.Attach(System.ServiceModel.OperationContext context)
+        {
+
+        }
+
+        void IAttachable.Detach(System.ServiceModel.OperationContext context)
+        {
+
+        }
+    }
+}
